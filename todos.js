575c97ac620f81f94e3dc07384c26e6044d794de
Alan Davis Babu <alanb@qburst.com>
2018-10-26 19:49:33 +0530

    To do list

---
+++ todos.js
@@ -0,0 +1,31 @@
+let todos = [{
+        item: 'Soap',
+        quantity: 15
+    },
+    {
+        item: 'Denim',
+        quantity: 3
+    },
+    {
+        item: 'Shirt',
+        quantity: 7
+    },
+    {
+        item: 'Boots',
+        quantity: 2
+    },
+    {
+        item: 'Candle',
+        quantity: 8
+    },
+    {
+        item: 'Phone',
+        quantity: 3
+    },
+    {
+        item: 'Laptop',
+        quantity: 1
+    }
+];
+
+export {todos};
